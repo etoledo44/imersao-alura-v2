646170885f02012ab32f06af6fcb6051358e1fa1
Erique Toledo <eriquetoledo@outlook.com>
2021-01-25 14:35:59 -0300

    imersao_v2_part1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Footer from '../src/Components/Footer'
+import GitHubCorner from '../src/Components/GitHubCorner'
+import QuizBackground from '../src/Components/QuizBackground'
+import QuizLogo from '../src/Components/QuizLogo'
+import Widget from '../src/Components/Widget'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage = {  db.bg[Math.floor(Math.random() * db.bg.length)]}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Imersão da hora</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Vamo que vamo</p>
+          </Widget.Content>
+        </Widget>
+        <GitHubCorner projectUrl='https://www.github.com/etoledo44' />
+        <Footer />
+      </QuizContainer>
+    </QuizBackground>
+  )
 }
---
+++ src/Components/QuizBackground/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const QuizBackground = styled.div`
+    width: 100%;
+    background-size: cover;
+    background-position: center;
+    background-image: url(${({backgroundImage}) => backgroundImage});
+    background-color: ${({theme})=> theme.colors.mainBg};
+    flex: 1;
+
+`
+
+export default QuizBackground
